4984dc4389b8eff2d44aede0b370a651ff763a23
vidhik2002 <vidhik2002@gmail.com>
2021-10-14 14:10:45 +0530

    feat: query Parameters

--- weather.js
+++ weather.js
@@ -4,5 +4,11 @@ const { key } = require("./env.json")
 
 function fetchWeather(city){
     console.log(city)
+    const queryParameters = {
+        appid: key,
+        q: city,
+    };  
+    const url = "https://api.openweathermap.org/data/2.5/weather?q="+queryParameters.q+"&appid="+queryParameters.appid; 
+    console.log(url)
 }
 module.exports = fetchWeather;
